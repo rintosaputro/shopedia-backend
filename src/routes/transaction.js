204d5248024e1f7f287a64cbcfeec5bc44fff7bf
Nabil Hardiansa <nhardiansa@gmail.com>
2022-03-26 15:19:47 +0800

    feat: add detail transaction

--- src/routes/transaction.js
+++ src/routes/transaction.js
@@ -5,12 +5,14 @@ const orderedProductController = require('../controllers/orderedProduct')
 
 const auth = require('../middlewares/auth')
 
-transaction.get('/', auth.verifyUser, transactionController.listTransaction)
-transaction.post('/', auth.verifyUserConfirmed, transactionController.createTransaction)
-
 // ordered product
 transaction.get('/ordered-product', auth.verifyUser, orderedProductController.listOrderedProduct)
 transaction.post('/ordered-product', auth.verifyUserConfirmed, orderedProductController.addOrderedProduct)
 transaction.patch('/change-order-status/:id', auth.verifyUserConfirmed, auth.verifySeller, orderedProductController.changeOrderStatus)
+
+// transaction
+transaction.get('/', auth.verifyUser, transactionController.listTransaction)
+transaction.get('/:id', auth.verifyUser, transactionController.detailTransaction)
+transaction.post('/', auth.verifyUserConfirmed, transactionController.createTransaction)
 
 module.exports = transaction
