5f1bb0dd734432cb653672d1dfc67ba22c5fb79e
Nabil Hardiansa <nhardiansa@gmail.com>
2022-03-27 07:15:08 +0800

    feat: add delete feature for ordered product

--- src/controllers/orderedProduct.js
+++ src/controllers/orderedProduct.js
@@ -8,6 +8,7 @@ const OrderStatus = require('../models/orderStatus')
 const Sequelize = require('sequelize')
 const validator = require('validator')
 const Users = require('../models/users')
+const Roles = require('../models/roles')
 
 exports.listOrderedProduct = async (req, res) => {
   try {
@@ -17,7 +18,8 @@ exports.listOrderedProduct = async (req, res) => {
     let { page, limit } = req.query
 
     const where = {
-      userId: id
+      userId: id,
+      deletedAt: null
     }
 
     if (orderStatusId) {
@@ -122,6 +124,8 @@ exports.addOrderedProduct = async (req, res) => {
       return responseHandler(res, 400, 'Product not found')
     }
 
+    data.storeId = product.storeId
+
     if (product.stock < qty) {
       return responseHandler(res, 400, 'Stock not enough')
     }
@@ -197,10 +201,6 @@ exports.changeOrderStatus = async (req, res) => {
         {
           model: OrderStatus,
           attributes: ['id', 'name']
-        },
-        {
-          model: Transactions,
-          attributes: ['id', 'total']
         }
       ],
       attributes: {
@@ -247,14 +247,10 @@ exports.changeOrderStatus = async (req, res) => {
         {
           model: OrderStatus,
           attributes: ['id', 'name']
-        },
-        {
-          model: Transactions,
-          attributes: ['id', 'total']
         }
       ],
       attributes: {
-        exclude: ['productId', 'transactionId']
+        exclude: ['productId']
       }
     })
 
@@ -268,7 +264,154 @@ exports.changeOrderStatus = async (req, res) => {
     } else if (err.name === 'SequelizeForeignKeyConstraintError') {
       return responseHandler(res, 400, 'You must provide registered input data')
     } else {
-      return responseHandler(res, 500, 'Error while add ordered product')
+      return responseHandler(res, 500, 'Error while change order status')
+    }
+  }
+}
+
+exports.listOrderedProductSeller = async (req, res) => {
+  try {
+    const { id: userId } = req.user
+
+    const { orderStatusId } = req.query
+    let { page, limit } = req.query
+
+    const seller = await Users.findByPk(userId, {
+      attributes: ['id', 'name', 'email', 'storeId']
+    })
+
+    const { storeId } = seller
+    console.log(storeId)
+
+    const where = {
+      storeId
+    }
+
+    if (orderStatusId) {
+      where.orderStatusId = orderStatusId
+    }
+
+    const include = [
+      {
+        model: Products,
+        as: 'product',
+        attributes: ['id', 'name', 'price', 'stock', 'storeId'],
+        include: [{
+          model: ProductImage,
+          attributes: ['id', 'image'],
+          limit: 1
+        }]
+      },
+      {
+        model: OrderStatus,
+        attributes: ['id', 'name']
+      }
+    ]
+
+    page = parseInt(page) || 1
+    limit = parseInt(limit) || 10
+
+    const offset = (page - 1) * limit
+
+    const { rows, count } = await OrderedProducts.findAndCountAll({
+      where,
+      attributes: {
+        exclude: ['userId', 'createdAt', 'updatedAt']
+      },
+      include,
+      limit,
+      offset
+    })
+
+    if (!rows.length) {
+      return responseHandler(res, 404, 'No ordered product found')
+    }
+
+    const data = {
+      ...req.query,
+      page,
+      limit
+    }
+
+    const path = req.originalUrl.split('?')[0]
+    const pageInfo = pageInfoCreator(count, path, data)
+
+    return responseHandler(res, 200, 'Ordered product list', rows, null, pageInfo)
+  } catch (err) {
+    console.error(err)
+
+    if (err.errors) {
+      const error = err.errors.map(err => ({ field: err.path, message: err.message }))
+      return responseHandler(res, 400, 'Check your input', null, error)
+    } else {
+      return responseHandler(res, 500, 'Error while get ordered product')
+    }
+  }
+}
+
+exports.deleteOrderedProduct = async (req, res) => {
+  try {
+    const { id: orderedProductId } = req.params
+    const { id: userId } = req.user
+
+    const user = await Users.findByPk(userId, {
+      include: [
+        {
+          model: Roles,
+          attributes: ['id', 'name']
+        }
+      ]
+    })
+
+    const orderedProduct = await OrderedProducts.findByPk(orderedProductId, {
+      include: [
+        {
+          model: OrderStatus,
+          attributes: ['id', 'name']
+        }
+      ]
+    })
+
+    if (!orderedProduct) {
+      return responseHandler(res, 400, 'Ordered product not found')
+    }
+
+    const { name: status } = orderedProduct.orderStatus
+
+    if (status !== 'Completed' && status !== 'Canceled') {
+      return responseHandler(res, 400, 'Only canceled or completed order can be deleted')
+    }
+
+    const roleName = user.role.name
+
+    if (roleName === 'user') {
+      if (userId !== orderedProduct.userId) {
+        return responseHandler(res, 400, 'You can only delete your ordered product')
+      }
+      orderedProduct.deletedAt = new Date()
+      await orderedProduct.save()
+      return responseHandler(res, 200, 'Ordered product deleted', orderedProduct)
+    } else if (roleName === 'seller') {
+      const storeId = user.storeId
+
+      if (storeId !== orderedProduct.storeId) {
+        return responseHandler(res, 400, 'You can only delete your ordered product')
+      } else {
+        await orderedProduct.destroy()
+        return responseHandler(res, 200, 'Ordered product deleted', orderedProduct)
+      }
+    }
+
+    // console.log(role.role.name)
+    return responseHandler(res, 200, 'no access')
+  } catch (err) {
+    console.error(err)
+
+    if (err.errors) {
+      const error = err.errors.map(err => ({ field: err.path, message: err.message }))
+      return responseHandler(res, 400, 'Check your input', null, error)
+    } else {
+      return responseHandler(res, 500, 'Error while get ordered product')
     }
   }
 }
